6ad968157a98acd5e86a9c391b06fe3b3857734a
brenoslivio <brenoslivio@pm.me>
2024-02-25 17:01:02 +0000

    Sticking with Streamlit.

--- Dash/pages/browse.py
+++
@@ -1,8 +0,0 @@
-import dash
-from dash import html, dcc, callback, Input, Output
-
-dash.register_page(__name__, title="BioMetaExplorer")
-
-layout = html.Div(
-    children=[html.H1(children="This is our upload page")]
-)
