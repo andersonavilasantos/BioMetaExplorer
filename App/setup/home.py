540eebcf0b524f6a063aed3da7bd7ed6afedfda9
brenoslivio <brenoslivio@pm.me>
2024-03-30 17:33:32 +0000

    Queue running sequentially.

--- App/setup/home.py
+++ App/setup/home.py
@@ -8,8 +8,36 @@ import streamlit.components.v1 as components
 import os
 from secrets import choice
 import string
+from queue import Queue
+from threading import Thread
+from streamlit.runtime.scriptrunner import add_script_run_ctx
 
+def submit_job(fasta_sequences, job_path):
+    with open(os.path.join(job_path, "predict.fasta"), "w") as f: 
+        for record in SeqIO.parse(fasta_sequences, "fasta"):
+            f.write(record.format("fasta"))
+
+    subprocess.run(["python", "../Classification/main.py", "--test", job_path] +
+                    ["--path_model", "../Classification/results/enc1_cnn_bilstm_4conv_k1_concat2_bio/model.h5"] +
+                    ["--encoding", "1", "--k", "1", "--concat", "1", "--feat_extraction", "1", "--features_exist", "1", "--output", job_path])
+
+def queue_listener():
+    while True:
+        if not job_queue.empty():
+            fasta_sequences, job_path = job_queue.get()
+            submit_job(fasta_sequences, job_path)
+            
 def runUI():
+    global job_queue
+
+    job_queue = Queue()
+
+    if "listener" not in st.session_state:
+        queue_thread = Thread(target=queue_listener)
+        add_script_run_ctx(queue_thread)
+        queue_thread.start()
+        st.session_state["listener"] = True
+
     img_cols = st.columns([3, 2, 3])
 
     with img_cols[1]:
@@ -42,33 +70,21 @@ def runUI():
 
     if submitted:
         if fasta_text:
-            job_id = ''.join([choice(string.ascii_uppercase + string.digits) for _ in range(10)])
+            job_id = ''.join([choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(10)])
             job_path = os.path.join(predict_path, job_id)
-            st.success(f"Job submitted successfully! You can consult the results in Jobs using the following ID: **{job_id}**")
-            os.makedirs(job_path)
-            
-            stringio = StringIO(fasta_text)
-            with open(os.path.join(job_path, "predict.fasta"), "w") as f: 
-                for record in SeqIO.parse(stringio, "fasta"):
-                    f.write(record.format("fasta"))
+            fasta_sequences = StringIO(fasta_text)
 
-            subprocess.run(["python", "../Classification/main.py", "--test", job_path] +
-                            ["--path_model", "../Classification/results/enc1_cnn_bilstm_4conv_k1_concat2_bio/model.h5"] +
-                            ["--encoding", "1", "--k", "1", "--concat", "1", "--feat_extraction", "1", "--features_exist", "1", "--output", job_path])
-            
+            os.makedirs(job_path)
+            job_queue.put((fasta_sequences, job_path))
+            st.success(f"Job submitted to the queue. You can consult the results in \"Jobs\" using the following ID: **{job_id}**")
+
         elif fasta_file:
-            job_id = ''.join([choice(string.ascii_uppercase + string.digits) for _ in range(10)])
+            job_id = ''.join([choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(10)])
             job_path = os.path.join(predict_path, job_id)
-            st.success(f"Job submitted successfully! You can consult the results in Jobs using the following ID: **{job_id}**")
+            fasta_sequences = StringIO(fasta_file.getvalue().decode("utf-8"))
+
             os.makedirs(job_path)
-
-            stringio = StringIO(fasta_file.getvalue().decode("utf-8"))
-            with open(os.path.join(job_path, "predict.fasta"), "w") as f: 
-                for record in SeqIO.parse(stringio, "fasta"):
-                    f.write(record.format("fasta"))
-
-            subprocess.run(["python", "../Classification/main.py", "--test", job_path] +
-                            ["--path_model", "../Classification/results/enc1_cnn_bilstm_4conv_k1_concat2_bio/model.h5"] +
-                            ["--encoding", "1", "--k", "1", "--concat", "1", "--feat_extraction", "1", "--features_exist", "1", "--output", job_path])
+            job_queue.put((fasta_sequences, job_path))
+            st.success(f"Job submitted to the queue. You can consult the results in \"Jobs\" using the following ID: **{job_id}**")
         else:
             st.error("No sequences submitted!")
